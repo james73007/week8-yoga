8383a84515587fe5b3ff93e2ab0f88b452471587
unknown <roger73007@gmail.com>
2021-08-25 10:26:15 +0800

    Update 2021-08-25T02:26:08.554Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,22 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  slidesPerColumn: 2,
+  spaceBetween: 30,
+  breakpoints: {
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      slidesPerColumn: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 992px
+    992: {
+      slidesPerView: 3,
+      slidesPerColumn: 2,
+      spaceBetween: 30
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
