fa7d8e9d125e976feb1640e6bc07a2f8af185aee
unknown <roger73007@gmail.com>
2021-08-31 14:58:45 +0800

    修改部分功能

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,23 +1,60 @@
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 1,
+//   spaceBetween: 30,
+//   autoplay:{
+//     delay:3500,
+//   },
+//   breakpoints: {
+//     768: {
+//       slidesPerView: 3,
+//       spaceBetween: 30
+//     },
+//     992:{
+//       slidesPerView: 4,
+//       spaceBetween: 30
+//     }
+//   }
+// });
+
+
+//datepicker日期套件
+const elem = document.querySelector('input[name="datepicker"]');
+const datepicker = new Datepicker(elem, {
+    buttonClass: 'btn primary',
+  nextArrow: '>',
+  prevArrow: '<',
+  todayHighlight:true, //highlight 今天的日期
+  // weekStart:0,
+  autohide:true,   //選完日期後會自動關閉
+
+}); 
+
+//首頁 聽聽他們怎麼說 swiper
 
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
-  slidesPerColumn: 2,
   spaceBetween: 30,
-  breakpoints: {
-    // when window width is >= 768px
-    768: {
-      slidesPerView: 2,
-      slidesPerColumn: 2,
-      spaceBetween: 20
-    },
-    // when window width is >= 992px
-    992: {
-      slidesPerView: 3,
-      slidesPerColumn: 2,
-
-      spaceBetween: 30
-    },
-   
-  }
-  
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
+
+//瑜珈空間 swiper
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 1,
+ 
+//   autoplay:{
+//     delay:3500,
+//   },
+//   breakpoints: {
+//     768: {
+//       slidesPerView: 3,
+      
+//     },
+//     992:{
+//       slidesPerView: 4,
+      
+//     }
+//   }
+// });
