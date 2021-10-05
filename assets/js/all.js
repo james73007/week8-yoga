6919d3fd80fa10d7df6cbe8c03f62b2d79b38fa9
unknown <roger73007@gmail.com>
2021-10-05 11:30:25 +0800

    Update 2021-10-05T03:30:20.441Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -24,7 +24,7 @@ var datepicker = new Datepicker(elem, {
 var commentSwiper = document.querySelector('.commentSwiper');
 
 if (commentSwiper) {
-  var swiper = new Swiper(".commentSwiper", {
+  var _swiper = new Swiper(".commentSwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
     autoplay: {
@@ -52,4 +52,35 @@ if (commentSwiper) {
     }
   });
 }
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2.2,
+      grid: {
+        rows: 2
+      }
+    },
+    992: {
+      slidesPerView: 3,
+      rows: 2
+    }
+  }
+});
+$('.re').on("click", function () {
+  $('.course-level').slideUp().slideDown();
+});
+$('.re').on("click", function () {
+  $('.course-level').slideUp().slideDown();
+});
+$(document).ready(function () {
+  // 在這撰寫javascript程式碼
+  $('.course-level').hide();
+  $('.hide').hide();
+});
 //# sourceMappingURL=all.js.map
