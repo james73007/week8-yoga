0ec27db23e4718590a3c2b532b1171268dd1b9cc
james232323 <roger73007@gmail.com>
2022-04-22 15:12:47 +0800

    Update 2022-04-22T07:12:33.364Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -5,28 +5,27 @@ AOS.init({
   // values from 0 to 3000, with step 50ms
   duration: 1200,
   // values from 0 to 3000, with step 50ms
-  once: false // whether animation should happen only once - while scrolling down
+  once: true // whether animation should happen only once - while scrolling down
 
 }); //datepicker日期套件
+// const elem = document.querySelector('input[name="datepicker"]');
+// const datepicker = new Datepicker(elem, {
+//     buttonClass: 'btn primary',
+//   nextArrow: '>',
+//   prevArrow: '<',
+//   todayHighlight:true, //highlight 今天的日期
+//   // weekStart:0,
+//   autohide:true,   //選完日期後會自動關閉
+// }); 
+//首頁 聽聽他們怎麼說 swiper
 
-var elem = document.querySelector('input[name="datepicker"]');
-var datepicker = new Datepicker(elem, {
-  buttonClass: 'btn primary',
-  nextArrow: '>',
-  prevArrow: '<',
-  todayHighlight: true,
-  //highlight 今天的日期
-  // weekStart:0,
-  autohide: true //選完日期後會自動關閉
-
-}); //首頁 聽聽他們怎麼說 swiper
-
-var commentSwiper = document.querySelector('.commentSwiper');
+var commentSwiper = document.querySelector('.js-commentSwiper');
+console.log(commentSwiper);
 
 if (commentSwiper) {
-  var _swiper = new Swiper(".commentSwiper", {
+  var swiper = new Swiper(".js-commentSwiper", {
     slidesPerView: 1,
-    spaceBetween: 30,
+    spaceBetween: 8,
     autoplay: {
       delay: 2500
     },
@@ -37,12 +36,14 @@ if (commentSwiper) {
     breakpoints: {
       768: {
         slidesPerView: 2.2,
+        spaceBetween: 30,
         grid: {
           rows: 2
         }
       },
       992: {
         slidesPerView: 3,
+        spaceBetween: 30,
         rows: 2
       }
     },
@@ -53,34 +54,62 @@ if (commentSwiper) {
   });
 }
 
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  autoplay: {
-    delay: 2500
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 2.2,
-      grid: {
-        rows: 2
-      }
+var teachersSwiper = document.querySelector('.js-swiper-teachersIntro');
+
+if (teachersSwiper) {
+  var _swiper = new Swiper('.js-swiper-teachersIntro', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    grid: {
+      rows: 4,
+      fill: 'column'
     },
-    992: {
-      slidesPerView: 3,
-      rows: 2
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        grid: {
+          rows: 1,
+          fill: 'row'
+        }
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        grid: {
+          rows: 1,
+          fill: 'row'
+        }
+      }
     }
-  }
-});
-$('.re').on("click", function () {
-  $('.course-level').slideUp().slideDown();
-});
-$('.re').on("click", function () {
-  $('.course-level').slideUp().slideDown();
-});
-$(document).ready(function () {
-  // 在這撰寫javascript程式碼
-  $('.course-level').hide();
-  $('.hide').hide();
-});
+  });
+}
+
+;
+var courseRecommend = document.querySelector('.js-course-swiper');
+console.log(courseRecommend);
+
+if (courseRecommend) {
+  var _swiper2 = new Swiper('.js-course-swiper', {
+    slidesPerView: 1.3,
+    spaceBetween: 30,
+    autoplay: {
+      delay: 3000
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      992: {
+        slidesPerView: 4.2,
+        spaceBetween: 30
+      }
+    }
+  });
+}
 //# sourceMappingURL=all.js.map
